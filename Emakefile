0bceb2b1769222c737e421af26ee7b7bc31199f1
Nimrod Dayan <nimrod.dayan@rovio.com>
2016-08-11 15:10:58 +0300

    Added build scripts

---
+++ Emakefile
@@ -0,0 +1,2 @@
+{'src/mod_http_offline', [{outdir, "ebin"},{i,"/usr/local/Cellar/ejabberd/16.08/lib/ejabberd-16.08/include"}]}.
+
---
+++ build.sh
@@ -0,0 +1,2 @@
+erl -pa /usr/local/Cellar/ejabberd/16.08/lib/lager-3.2.1/ebin/ -pa /usr/local/Cellar/ejabberd/16.08/lib/fast_xml-1.1.14/ebin/ -make
+
